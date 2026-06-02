5e306074a2f96413e58b2027862ac7fe3bf9cce1
Kirti <162712321+Kirti391@users.noreply.github.com>
2026-06-02 21:30:33 +0530

    Update app.js

--- Todo app/app.js
+++ Todo app/app.js
@@ -14,15 +14,23 @@ btn.addEventListener("click", function () {
     let taskText = document.createElement("span");
     taskText.innerText = inp.value;
 
+    // MARK DONE BUTTON
+    let doneBtn = document.createElement("button");
+    doneBtn.innerText = "Done";
+    doneBtn.classList.add("done");
+
+    // EDIT BUTTON
     let editBtn = document.createElement("button");
     editBtn.innerText = "Edit";
     editBtn.classList.add("edit");
 
+    // DELETE BUTTON
     let delBtn = document.createElement("button");
     delBtn.innerText = "Delete";
     delBtn.classList.add("delete");
 
     item.appendChild(taskText);
+    item.appendChild(doneBtn);
     item.appendChild(editBtn);
     item.appendChild(delBtn);
 
@@ -31,6 +39,7 @@ btn.addEventListener("click", function () {
     inp.value = "";
 });
 
+/* EVENT DELEGATION */
 
 ul.addEventListener("click", function (event) {
 
@@ -62,5 +71,19 @@ ul.addEventListener("click", function (event) {
         ) {
             taskSpan.innerText = updatedTask;
         }
+    }
+
+    // MARK DONE
+    if (event.target.classList.contains("done")) {
+
+        let listItem = event.target.parentElement;
+
+        let taskSpan = listItem.querySelector("span");
+
+        taskSpan.style.textDecoration = "line-through";
+        taskSpan.style.color = "gray";
+
+        event.target.innerText = "Completed";
+        event.target.disabled = true;
     }
 });
